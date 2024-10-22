5bb0b4e645b8d9023e9ab26cfc4519c2283487ae
Meyly <meylymerma03@gmail.com>
2024-10-21 19:34:54 -0500

    Nuevo

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span className='text-muted'>
+                <p>2024</p>
+            </span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent;
